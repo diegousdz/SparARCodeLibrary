82116dd0694d9d83e9a43bff3a899b7e0e61f362
Diego Aguirre <vfxartist.diegoaguirre@gmail.com>
2020-09-05 17:06:36 -0300

    FaceDetection
    face detection script

--- FaceDetection.js
+++ FaceDetection.js
@@ -1,27 +1,23 @@
-
 const Scene = require('Scene');
 const FaceTracking = require('FaceTracking');
 export const D = require('Diagnostics');
 
 (async function() {
- const [monster,circle,fireUp,fireDown] = await Promise.all([
-   Scene.root.findFirst('MonsterIcon'),
-   Scene.root.findFirst('GradientCircle'),
+ const [circle,fireUp,fireDown] = await Promise.all([
+   Scene.root.findFirst('Lut+Circle'),
    Scene.root.findFirst('Fuego Arriba'),
    Scene.root.findFirst('Fuego Abajo')
  ]);
 D.watch("face count: ", FaceTracking.face(0).isTracked);
 
-//Check if face is found - If face is found hide the Monster Icon image
+//Check if face is found - If face is found hide the things inside the if
 FaceTracking.face(0).isTracked.monitor().subscribe(function(e) {
     if (e.newValue) {
- 	 monster.hidden = true;
- 	 circle.hidden = false;
  	 fireUp.hidden = false;
  	 fireDown.hidden = false;
+  	 circle.hidden = false;
     }
     else {
-	 monster.hidden = false;
 	 circle.hidden = true;
  	 fireUp.hidden = true;
  	 fireDown.hidden = true;
